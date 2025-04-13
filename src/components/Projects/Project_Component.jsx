4697ef25bfa30ceae809be2bf247e58321bf8af2
PRIYANSH SAXENA <132459333+Priyansh7999@users.noreply.github.com>
2025-04-13 12:30:24 +0530

    Update Project_Component.jsx

--- src/components/Projects/Project_Component.jsx
+++ src/components/Projects/Project_Component.jsx
@@ -19,7 +19,7 @@ export default function Project_Component({ activeField }) {
             technologies: ["React JS", "Node js", "CSS", "Firebase"],
             src: trip_planner,
             github: "https://github.com/Priyansh7999/Trip-Advisor",
-            live: "https://trip-advisor-umber.vercel.app/"
+            live: "https://trip-advisor-anzz.vercel.app/"
         },
         {
             name: "Weather App",

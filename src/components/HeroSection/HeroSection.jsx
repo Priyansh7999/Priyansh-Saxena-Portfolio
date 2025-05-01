45198e4504eca87524c1ecab9f9a0a9d2f536dbc
PRIYANSH SAXENA <132459333+Priyansh7999@users.noreply.github.com>
2025-05-01 12:23:27 +0530

    Update HeroSection.jsx

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -30,7 +30,7 @@ export default function HeroSection() {
                 <h1 id='name'>Priyansh Saxena</h1>
                 <p id='description'>Frontend Developer at heart, Software Engineer by passion—Building fun and functional digital experiences!</p>
                 <div style={{ display: "flex", gap: "30px", justifyContent: "center" }}>
-                    <a href="/Priyansh_Saxena_WeV_Developer.pdf" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1Li4ztm-4jKndU8Rw-8rOSIm7BnHb5oV6/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
                         <button
                             className={styles.button}
                             type="button"
